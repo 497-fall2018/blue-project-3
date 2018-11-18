c6a24f79a0b9c980a9fcb929f553f0348ca0e078
nlakshmanan <nnlnachu@gmail.com>
2018-11-18 13:22:18 -0600

    Integrating Robbie and Sumin's Centroid logic with google maps

--- BlueNearUs/App.js
+++ BlueNearUs/App.js
@@ -1,46 +1,103 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { Marker,PROVIDER_GOOGLE } from 'react-native-maps'
 
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-           provider={PROVIDER_GOOGLE}
-           style={styles.map}
-           region={{
-           latitude: 37.78825,
-           longitude: -122.4324,
-           latitudeDelta: 0.015,
-           longitudeDelta: 0.0121,
-         }}
-        ></MapView>
-      </View>
-    );
-  }
-}
 
-function rad2degr(rad) { return rad * 180 / Math.PI; }
-function degr2rad(degr) { return degr * Math.PI / 180; }
-function getLatLngCenter(latLngInDegr) {
+const styles=StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height:400,
+    width:400,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+
+export default class App extends Component<Props> {
+  
+  state = {
+    latitude: null,
+    longitude: null,
+    user_lat:42.057989,
+    user_long:-87.675641,
+    error: null,
+    region: {
+      latitude: 42.055214,
+      longitude: -87.674894,
+      latitudeDelta: 0.0222,
+      longitudeDelta: 0.0221,
+    },
+    markers: [
+      {
+        key: '1',
+        coordinate: {
+          latitude: 42.053472, 
+          longitude: -87.672652,
+        },
+        title: "Person A",
+        description: "This is the location of Person A",
+      },
+      {
+        key: '2',
+        coordinate: {
+          latitude: 42.058053, 
+          longitude: -87.675137, 
+        },
+        title: "Person B",
+        description: "This is the location of Person B",
+      },
+      {
+        key: '3',
+        coordinate: {
+          latitude: 42.067079,  
+          longitude: -87.692223,
+        },
+        title: "Person C",
+        description: "This is the location of Person C",
+      },
+    ],
+
+    centroid: [{
+      key: '0',
+      coordinate: {
+          latitude: 42.062245,  
+          longitude: -87.677697, 
+        },
+        title: "Your Optimized Hangout Spot!",
+        pinColor: "#8B008B",
+        description: "This is the centroid of your locations! :)",
+    }],
+    your_location: [{
+      key: '99',
+      coordinate: {
+          latitude: 42.057989,   
+          longitude: -87.675641, 
+        },
+        title: "Your Location",
+        pinColor: "#00ff00",
+        description: "This is where you are!",
+    }],
+
+    
+  };
+
+  rad2degr(rad) { return rad * 180 / Math.PI; }
+  degr2rad(degr) { return degr * Math.PI / 180; }
+
+//Norris: 42.053472, -87.672652
+//Tech: 42.058053, -87.675137
+//Mudd: 42.058320, -87.674434
+//Welsh-Ryan: 42.067079, -87.692223
+//E2: 42.052071, -87.684719
+//EP: 42.049022, -87.677566
+//The Garage: 42.059412, -87.673223
+  getLatLngCenter(latLngInDegr) {
     var LATIDX = 0;
     var LNGIDX = 1;
     var sumX = 0;
@@ -48,8 +105,8 @@ function getLatLngCenter(latLngInDegr) {
     var sumZ = 0;
 
     for (var i=0; i<latLngInDegr.length; i++) {
-        var lat = degr2rad(latLngInDegr[i][LATIDX]);
-        var lng = degr2rad(latLngInDegr[i][LNGIDX]);
+        var lat = this.degr2rad(latLngInDegr[i][LATIDX]);
+        var lng = this.degr2rad(latLngInDegr[i][LNGIDX]);
         // sum of cartesian coordinates
         sumX += Math.cos(lat) * Math.cos(lng);
         sumY += Math.cos(lat) * Math.sin(lng);
@@ -65,38 +122,96 @@ function getLatLngCenter(latLngInDegr) {
     var hyp = Math.sqrt(avgX * avgX + avgY * avgY);
     var lat = Math.atan2(avgZ, hyp);
 
-    return ([rad2degr(lat), rad2degr(lng)]);
+    return ([this.rad2degr(lat), this.rad2degr(lng)]);
 }
 
-//Norris: 42.053472, -87.672652
-//Tech: 42.058053, -87.675137
-//Mudd: 42.058320, -87.674434
-//Welsh-Ryan: 42.067079, -87.692223
-//E2: 42.052071, -87.684719
-//EP: 42.049022, -87.677566
-//The Garage: 42.059412, -87.673223
-var latLngInDegr=[[42.053472, -87.672652],[42.058053, -87.675137],[42.058320, -87.674434],[42.067079, -87.692223],[42.052071, -87.684719], [42.049022, -87.677566],[42.059412, -87.673223]];
-var centroid=getLatLngCenter(latLngInDegr);
-console.log(centroid[0],centroid[1]);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
+  calculateCentroid() {
+    var coordinates=[];
+    var answer;
+    // this.state.markers.map(m => (
+    //     coordinates.push({m.coordinate});
+    //   ));
+    var i;
+    for (i=0;i<this.state.markers.length;i++) {
+      coordinates.push([this.state.markers[i].coordinate.latitude,this.state.markers[i].coordinate.longitude]);
+    }
+    if (this.state.latitude!=null && this.state.latitude!=null){
+    coordinates.push([this.state.user_lat, this.state.user_long]);}
+    console.log(coordinates);
+    answer=this.getLatLngCenter(coordinates);
+    console.log(answer[0],answer[1]);
+    return answer;
+  }
+
+
+  render() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+    console.log("This is your location...");
+    console.log(this.state.latitude,this.state.longitude);
+    centroid_coords=this.calculateCentroid();
+    const latlng={
+    latitude: centroid_coords[0],
+    longitude: centroid_coords[1],
+}
+    const latlng2= {
+      latitude: this.state.latitude,
+      longitude: this.state.longitude,
+    }
+    return (
+
+      
+        <MapView
+           provider={PROVIDER_GOOGLE}
+           style={styles.map}
+           initialRegion={this.state.region}
+           showsUserLocation={true}
+           showsCompass={true}
+           >
+    
+      {this.state.markers.map(marker => (
+        <Marker
+          key = {marker.key}
+          coordinate={marker.coordinate}
+          title={marker.title}
+          description={marker.description}
+        />
+      ))}
+
+      {this.state.centroid.map(c => (
+        <Marker
+          key = {c.key}
+          coordinate={latlng}
+          title={c.title}
+          description={c.description}
+          pinColor={c.pinColor}
+        />
+      ))}
+
+      {this.state.your_location.map(y => (
+        <Marker
+          key = {y.key}
+          coordinate={y.coordinate}
+          title={y.title}
+          description={y.description}
+          pinColor={y.pinColor}
+        />
+      ))}
+    
+    </MapView>
+   
+
+
+    );
+  }
+}
