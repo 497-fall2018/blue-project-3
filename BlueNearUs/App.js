90fc9a00f14eae371bb66202a7e542bc823ef3a1
Sumin <suyou2021@u.northwestern.edu>
2018-11-28 10:41:45 -0600

    Card replaced with component

--- BlueNearUs/App.js
+++ BlueNearUs/App.js
@@ -272,69 +272,8 @@ export default class App extends Component<Props> {
 
             </View>
             <Content>
-              <Card>
-                <CardItem>
-                  <Left>
-                    <Thumbnail source={{ uri: 'http://www.stickpng.com/assets/images/5842997fa6515b1e0ad75adf.png' }} />
-                    <Body>
-                      <Text>NativeBase</Text>
-                      <Text note>GeekyAnts</Text>
-                    </Body>
-                  </Left>
-                </CardItem>
-                <CardItem cardBody>
-                  <Image source={{ uri: 'https://www.dairyherd.com/sites/default/files/Pan%20Pizza%20Hut.jpg' }} style={{ height: 200, width: null, flex: 1 }} />
-                </CardItem>
-                <CardItem>
-                  <Left>
-                    <Button transparent>
-                      {/* <Icon active name="thumbs-up" /> */}
-                      <Text>12 Likes</Text>
-                    </Button>
-                  </Left>
-                  <Body>
-                    <Button transparent>
-                      {/* <Icon active name="chatbubbles" /> */}
-                      <Text>4 Comments</Text>
-                    </Button>
-                  </Body>
-                  <Right>
-                    <Text>11h ago</Text>
-                  </Right>
-                </CardItem>
-
-              </Card>
-              <Card>
-                <CardItem>
-                  <Left>
-                    <Thumbnail source={{ uri: 'http://www.stickpng.com/assets/images/5842997fa6515b1e0ad75adf.png' }} />
-                    <Body>
-                      <Text>NativeBase</Text>
-                      <Text note>GeekyAnts</Text>
-                    </Body>
-                  </Left>
-                </CardItem>
-                <CardItem cardBody>
-                  <Image source={{ uri: 'https://www.dairyherd.com/sites/default/files/Pan%20Pizza%20Hut.jpg' }} style={{ height: 200, width: null, flex: 1 }} />
-                </CardItem>
-                <CardItem>
-                  <Left>
-                    <Button transparent>
-                      {/* <Icon active name="thumbs-up" /> */}
-                      <Text>12 Likes</Text>
-                    </Button>
-                  </Left>
-                  <Body>
-                    <Button transparent>
-                      {/* <Icon active name="chatbubbles" /> */}
-                      <Text>4 Comments</Text>
-                    </Button>
-                  </Body>
-                  <Right>
-                    <Text>11h ago</Text>
-                  </Right>
-                </CardItem>
-              </Card>
+              <ResultCard/>
+              <ResultCard/>
               <ResultCard/>
             </Content>
           </View>
